accbe36cf499ad59cb9698ea498f9ac994c80d48
Guilherme Lopes <guilhermxlopes@gmail.com>
2023-04-19 00:57:55 -0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
+import { BookmarkModule } from './entities/bookmark/bookmark.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './entities/user/user.module';
 import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, DbModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    DbModule,
+  ],
 })
 export class AppModule {}
