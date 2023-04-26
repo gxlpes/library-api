3f8e737a43783d37e0a809c009d7527e635c7624
Guilherme Lopes <guilhermxlopes@gmail.com>
2023-04-25 23:47:01 -0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
 import { BookmarkModule } from './entities/bookmark/bookmark.module';
-import { UserModule } from './entities/user/user.module';
 import { UserController } from './entities/user/user.controller';
+import { UserModule } from './entities/user/user.module';
 
 @Module({
   imports: [
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from 'src/strategy/jwt.strategy';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
